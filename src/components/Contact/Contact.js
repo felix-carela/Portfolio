431f6e1005fe2a95eacbac21187a0ada3f21e223
felix-carela <felixacarela@protonmail.com>
2023-12-22 13:09:16 -0500

    Update contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,22 +4,28 @@ import styles from './Contact.module.css';
 const Contact = () => {
   return (
     <div id="contact" className={styles.contact}>
-      <div className="container">
-        <h1 className="contact-title">Contact Me</h1>
-        <p>felixacarela@protonmail.com</p>
-        <p>
-          <a href="https://www.linkedin.com/in/felix-carela/" target="_blank" rel="noopener noreferrer">
-            <img src="/images/LinkedIn.png" alt="LinkedIn" style={{ maxWidth: '50px' }} />
+      <div className={styles.container}>
+        <div className={styles.leftColumn}>
+          <a href="data/Felix Carela Resume.pdf" download className={styles.btn}>
+            Download Resume
           </a>
-        </p>
-        <p>
-          <a href="https://github.com/felix-carela" target="_blank" rel="noopener noreferrer">
-            <img src="/images/GitHub.png" alt="GitHub" style={{ maxWidth: '50px' }} />
-          </a>
-        </p>
-        <a href="data/Felix Carela Resume.pdf" download className="btn">
-          Download Resume
-        </a>
+        </div>
+        <div className={styles.rightColumn}>
+          <div className={styles.contactHeader}>
+            <h1 className={styles.contactTitle}>Contact Me:</h1>
+            <div className={styles.contactInfo}>
+              <p className={styles.email}>felixacarela@protonmail.com</p>
+              <div className={styles.socialLinks}>
+                <a href="https://www.linkedin.com/in/felix-carela/" target="_blank" rel="noopener noreferrer">
+                  <img src="/images/LinkedIn.png" alt="LinkedIn" className={styles.socialIcon} />
+                </a>
+                <a href="https://github.com/felix-carela" target="_blank" rel="noopener noreferrer">
+                  <img src="/images/GitHub.png" alt="GitHub" className={styles.socialIcon} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,13 @@ import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <div className={styles.header}> {/* Use styles.header for the class */}
-      <div className={styles.container}> {/* Use styles.container for the class */}
-        <a href="http://felixcarela.com" className={styles.logoLink}> {/* Use styles.logo for the class */}
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <a href="http://felixcarela.com" className={styles.logoLink}>
           <img src="/images/logo.png" alt="Logo" className={styles.logo}/>
         </a>
         <nav>
-          <ul className={styles.menu}> {/* Use styles.menu for the class */}
+          <ul className={styles.menu}>
             <li><a href="#about"><span>About</span></a></li>
             <li><a href="#skills"><span>Skills</span></a></li>
             <li><a href="#projects"><span>Projects</span></a></li>
