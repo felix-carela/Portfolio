28ae6767aa13babd53d9f9960cab6125337393ee
felix-carela <felixacarela@protonmail.com>
2023-12-21 19:27:16 -0500

    Update skills

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 // src/components/Header/Header.js
 
 import React from 'react';
-import styles from './Header.module.css';
+import styles from './Header.module.css'; // Import the CSS module
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <div className="container" id="section">
+    <div className={styles.header}> {/* Use styles.header for the class */}
+      <div className={styles.container}> {/* Use styles.container for the class */}
         <nav>
-          <ul id="menu">
+          <ul className={styles.menu}> {/* Use styles.menu for the class */}
             <li><a href="#about"><span>About</span></a></li>
             <li><a href="#skills"><span>Skills</span></a></li>
             <li><a href="#projects"><span>Projects</span></a></li>
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -5,9 +5,63 @@ import styles from './Skills.module.css';
 
 const Skills = () => {
   return (
-    <div id={styles.skills}>
+    <div id="skills" className={styles.skills}>
       <div className="container">
-        {/* ... Your Skills content ... */}
+        <h1 className="sub-title">Skills</h1>
+        <div className={styles.skillsList}>
+          <img
+            src="https://img.shields.io/badge/-Python-black?style=flat-square&logo=python"
+            alt="Python"
+          />
+          <img
+            src="https://img.shields.io/badge/-JavaScript-black?style=flat-square&logo=javascript"
+            alt="JavaScript"
+          />
+          <img
+            src="https://img.shields.io/badge/-SQL-black?style=flat-square&logo=postgresql"
+            alt="SQL"
+          />
+          <img
+            src="https://img.shields.io/badge/-React-black?style=flat-square&logo=react"
+            alt="React"
+          />
+        </div>
+        <div className="skills-slide">
+          <img
+            src="https://img.shields.io/badge/-Django-black?style=flat-square&logo=django"
+            alt="Django"
+          />
+          <img
+            src="https://img.shields.io/badge/-C++-black?style=flat-square&logo=c%2B%2B"
+            alt="C++"
+          />
+          <img
+            src="https://img.shields.io/badge/-Java-black?style=flat-square&logo=java"
+            alt="Java"
+          />
+        </div>
+        <img
+          src="https://img.shields.io/badge/-MongoDB-black?style=flat-square&logo=mongodb"
+          alt="MongoDB"
+        />
+        <img
+          src="https://img.shields.io/badge/-Express-black?style=flat-square&logo=express"
+          alt="Express"
+        />
+        <img
+          src="https://img.shields.io/badge/-TypeScript-black?style=flat-square&logo=typescript"
+          alt="TypeScript"
+        />
+        <div className="skills-slide">
+          <img
+            src="https://img.shields.io/badge/-HTML5-black?style=flat-square&logo=html5"
+            alt="HTML5"
+          />
+          <img
+            src="https://img.shields.io/badge/-CSS3-black?style=flat-square&logo=css3"
+            alt="CSS3"
+          />
+        </div>
       </div>
     </div>
   );
